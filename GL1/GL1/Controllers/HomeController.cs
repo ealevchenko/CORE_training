f92ccc36d490063d0cb7104ee2d48aa847a4840b
eduard <Lev75.eduard@gmail.com>
2022-07-12 13:03:28 +0300

    Добавил главу №1

---
+++ GL1/GL1/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using GL1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GL1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            return View("МyView");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else {
+                return View();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ViewResult ListResponses() { 
+        return View(Repository.Responses.Where(r=>r.WillAttend == true));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GL1/GL1/Models/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GL1.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Пожалуйста введите свое имя")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Пожалуйста введите свой адрес")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter а valid email address")] 
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста укажите свой телефон")]
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
