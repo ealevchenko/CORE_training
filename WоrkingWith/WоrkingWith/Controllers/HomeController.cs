dfb00ddfd5f5bf2456781431291cd23fd50e17b6
eduard <Lev75.eduard@gmail.com>
2022-08-05 12:18:12 +0300

    Добавил проект WоrkingWith

---
+++ WоrkingWith/WоrkingWith/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WоrkingWith.Models;
+
+namespace WоrkingWith.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products);
+    }
+}
---
+++ WоrkingWith/WоrkingWith/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace WоrkingWith.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
